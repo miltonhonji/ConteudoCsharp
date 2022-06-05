20d9e9488d0ce98c596955e8bc0b90e14216fc42
Milton Honji <factorysolutionIT@outlook.com>
2022-06-05 17:50:55 -0300

    Treinando Orientação a Obejetos Carros

---
+++ ProgramacaoOrientadosAObjetosComCarros/Model/Car.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ProgramacaoOrientadosAObjetosComCarros.Model
+{
+    public class Car
+    {
+        public Guid IdCar { get; set; }
+        public string BoardCar { get; set; }
+        public string BrandCar { get; set; }
+        public string YearCar { get; set; }
+        public int HorsePowerCar { get; set; }
+        public decimal WeightCar { get; set; }
+        public decimal MaximiumTorqueCar { get; set; }
+        public decimal LenghtCar { get; set; }
+        public decimal WidthCar { get; set; }
+        public decimal HeightCar { get;set; }
+        public decimal EngineCapacity { get; set; }
+        public decimal PrizeCar { get; set; }       
+    }
+}
