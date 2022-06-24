693d4a9d61d5ba23e65f5ef2e60e5fff44f4b9e0
Milton Honji <factorysolutionIT@outlook.com>
2022-06-24 17:57:10 -0300

    Adicionando opções para o menu

--- TreinamentoProgramacaoOrientadoAObjeto/Herança/ExemplosHeranca.cs
+++ TreinamentoProgramacaoOrientadoAObjeto/Herança/ExemplosHeranca.cs
@@ -37,6 +37,23 @@ namespace TreinamentoProgramacaoOrientadoAObjeto.Herança
 
             Console.Clear();
             Console.WriteLine("Banco dos Dev´s - Conta Corrente");
+            Console.WriteLine();
+            Console.WriteLine("Escolha uma opção: ");
+            Console.WriteLine("1 - Cadastrar Conta Corrente");
+            Console.WriteLine("2 - Cadastrar o Pix");
+            Console.WriteLine("3 - Obter Extrato Conta Corrente");
+            Console.WriteLine("4 - Atualizar Informações do Cliente");
+            Console.WriteLine("5 - Cancelar a Conta");
+            Console.WriteLine("6 - Fazer Depósito");
+            Console.WriteLine("7 - Sair");
+            int opcaoContaCorrente = int.Parse(Console.ReadLine());
+
+            switch (opcaoContaCorrente)
+            {
+                case 1: contaCorrenteRepository.Save(contaCorrente); break;
+                default: System.Environment.Exit(1); break;
+            }
+            
         }
     }
 }
