da593f0983f3291ca6c0ba8b8978a08142b0a212
Milton Honji <factorysolutionIT@outlook.com>
2022-06-30 18:06:19 -0300

    Adiciconando novos conteúdos de Polimorfismo

--- TreinamentoProgramacaoOrientadoAObjeto/Polimorfismo/ExemplosPolimorfismo.cs
+++ TreinamentoProgramacaoOrientadoAObjeto/Polimorfismo/ExemplosPolimorfismo.cs
@@ -8,17 +8,26 @@ namespace TreinamentoProgramacaoOrientadoAObjeto.Polimorfismo
         public void Execucao()
         {
             Console.Clear();
-            Console.WriteLine("Digite o primeiro valor: ");
+            Console.WriteLine("Digite o primeiro número: ");
             var numero1 = int.Parse(Console.ReadLine());
             Console.WriteLine();
-            Console.WriteLine("Digite o segundo valor: ");
+
+            Console.WriteLine("Digite o segundo número: ");
             var numero2 = int.Parse(Console.ReadLine());
             Console.WriteLine();
+
             Console.WriteLine("Digite o terceiro valor: ");
             var numero3 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            
+            var primeiraSoma = Somar(numero1, numero2);
+            var segundaSoma = Somar(numero1, numero2, numero3);
+            var terceiraSoma = Somar(true);
 
-            var chamarMetodo1 = Somar(numero1, numero2);
-            var chamarMetodo2 = Somar(numero1, numero2, numero3);
+            Console.WriteLine($" Primeira Soma: { primeiraSoma }.");
+            Console.WriteLine($" Segunda Soma: { segundaSoma }.");
+            Console.WriteLine($" Terceira Soma: { terceiraSoma }.");
+            Console.ReadKey();
         }
 
         public int Somar(int numeroUm, int numeroDois)
