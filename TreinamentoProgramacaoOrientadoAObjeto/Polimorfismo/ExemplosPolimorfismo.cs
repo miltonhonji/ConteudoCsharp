1f0de43010c713bb328bba002407adb451e2c76a
Milton Honji <factorysolutionIT@outlook.com>
2022-06-30 12:47:29 -0300

    Polimorfismo

--- TreinamentoProgramacaoOrientadoAObjeto/Polimorfismo/ExemplosPolimorfismo.cs
+++ TreinamentoProgramacaoOrientadoAObjeto/Polimorfismo/ExemplosPolimorfismo.cs
@@ -9,10 +9,16 @@ namespace TreinamentoProgramacaoOrientadoAObjeto.Polimorfismo
         {
             Console.Clear();
             Console.WriteLine("Digite o primeiro valor: ");
-            var numeroUm = int.Parse(Console.ReadLine());
+            var numero1 = int.Parse(Console.ReadLine());
             Console.WriteLine();
             Console.WriteLine("Digite o segundo valor: ");
-            var numeroDois = int.Parse(Console.ReadLine());
+            var numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Digite o terceiro valor: ");
+            var numero3 = int.Parse(Console.ReadLine());
+
+            var chamarMetodo1 = Somar(numero1, numero2);
+            var chamarMetodo2 = Somar(numero1, numero2, numero3);
         }
 
         public int Somar(int numeroUm, int numeroDois)
@@ -20,6 +26,25 @@ namespace TreinamentoProgramacaoOrientadoAObjeto.Polimorfismo
             var result = numeroUm + numeroDois;
 
             return result;
+        }
+
+        public int Somar(int numeroUm, int numeroDois, int numeroTres)
+        {
+            var result = numeroUm + numeroDois + numeroTres;
+
+            CalcularAlgumaCoisa();
+
+            return result; 
+        }
+
+        public int Somar(bool verifica)
+        {
+            return 1;
+        }
+
+        public void CalcularAlgumaCoisa()
+        {
+
         }
     }
 }
